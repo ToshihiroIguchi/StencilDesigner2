d1393c5e0ada9131d8ac8e873f2a971483f2ab43
Toshihiro Iguchi <toshihiro.iguchi.mail@gmail.com>
2026-04-19 15:11:02 +0900

    Cleanup: Prepare repository for Production Release V1.0 (Archives & Cleanup)

--- scratch_maker.mjs
+++
@@ -1,8 +0,0 @@
-import makerjs from 'makerjs';
-
-const line1 = new makerjs.paths.Line([0,0], [10,0]);
-const line2 = new makerjs.paths.Line([10,0], [10,10]);
-const f = makerjs.path.fillet(line1, line2, 2);
-console.log('Arc', f);
-// Can we ask for arc endpoints?
-console.log('Arc points?', makerjs.point.fromArc(f));
